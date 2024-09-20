b9650b88079cc09d1a14a5b48ae54af88e1e9f16
SA2724 <sa2724@njit.edu>
2024-09-20 12:16:57 -0400

    added multiplication test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,13 @@
 ''' My Calculator Tests'''
-from app.main import addition, subtraction
+from app.main import addition, multiplication, subtraction
 def test_addition():
     '''Addition Function'''
     assert addition(1,1) == 2
-
-
 def test_subtraction():
     '''Subtraction Function'''
     assert subtraction(1,1) == 0
+
+def test_multiplication():
+    '''Multiplication Function'''
+    assert multiplication(1,2) == 2
+    
