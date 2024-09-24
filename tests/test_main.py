9c85db6b372aee0e425ba2f09d6f2d90792e668d
SA2724 <sa2724@njit.edu>
2024-09-24 10:33:07 -0400

    fixed isssues

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,21 @@
 ''' My Calculator Tests'''
-from app.main import addition, subtraction
+import pytest
+from app.main import addition, division, multiplication, subtraction
+
 def test_addition():
     '''Addition Function'''
     assert addition(1,1) == 2
 
-
 def test_subtraction():
     '''Subtraction Function'''
     assert subtraction(1,1) == 0
+def test_multiplication():
+    '''Multiplication Function'''
+    assert multiplication(1,2) == 2
+def test_positive_division ():
+    '''Positive Division Test'''
+    assert division(1,1) == 1
+def test_negative_divison():
+    '''Negative Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10,0)
